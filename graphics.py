0ebe3b97bdc1228f63088694ec02133a7467654f
Sl-L <zeroscsps@gmail.com>
2025-07-12 18:27:40 -0300

    Added power center based multilateration, confidence ellipses and made some QoL related optimizations

--- graphics.py
+++ graphics.py
@@ -3,6 +3,7 @@ import scipy
 import random
 import math
 import numpy as np
+import itertools
 
 # pygame setup
 pygame.init()
@@ -118,7 +119,7 @@ class ParticleFilter:
             particles.append(pygame.Vector2(x, y))
         return particles
     
-    def predict(self, motion_std=5.0):
+    def predict(self, motion_std: float = 5.0):
         # Add small random motion to particles (simulates robot movement)
         for i in range(self.num_particles):
             self.particles[i].x += random.gauss(0, motion_std)
@@ -175,18 +176,41 @@ class ParticleFilter:
         return self.estimated_pos
     
     def update(self):
-        self.predict()
+        # No prediction is done since no walking is modeled
+        # self.predict()
+
         self.update_weights()
         self.resample()
         return self.estimate_position()
     
-    def draw(self, surface):
+    def draw_particles(self, surface: pygame.Surface):
         # Draw particles
         for particle in self.particles:
             pygame.draw.circle(surface, (100, 100, 255, 50), (int(particle.x), int(particle.y)), 2)
+
+    def draw_estimate(self, surface: pygame.Surface):
+        # Draw estimated position and confidence ellipse
+        particles = np.array([list(i) for i in self.particles]).T
+
+        cov = np.cov(particles, aweights=self.weights)
+
+        eigenvalues, eigenvectors = np.linalg.eigh(cov)
+        order = eigenvalues.argsort()[::-1]
+        eigenvalues, eigenvectors = eigenvalues[order], eigenvectors[:, order]
         
-        # Draw estimated position
-        pygame.draw.circle(surface, (255, 0, 0), (int(self.estimated_pos.x), int(self.estimated_pos.y)), 10, 2)
+        # Confidence scaling
+        k = np.sqrt(-2 * np.log(1 - 0.95))  # For 2D (chi-squared)
+        a = k * np.sqrt(eigenvalues[0])
+        b = k * np.sqrt(eigenvalues[1])
+        angle = np.degrees(np.arctan2(eigenvectors[1,0], eigenvectors[0,0]))
+
+        ellipse_surf = pygame.Surface((a, b), pygame.SRCALPHA)
+        pygame.draw.ellipse(ellipse_surf, "red", ellipse_surf.get_rect(), 2)
+        rotated_surf = pygame.transform.rotate(ellipse_surf, -angle)
+        rotated_rect = rotated_surf.get_rect(center=(estimated_pos.x, estimated_pos.y))
+        surface.blit(rotated_surf, rotated_rect.topleft)
+
+        pygame.draw.circle(map, "red", (estimated_pos.x, estimated_pos.y), 4)
 
 class UI:
     def __init__(self, base: pygame.Surface, map: pygame.Surface):
@@ -266,6 +290,87 @@ class UI:
                 blit_pos = beacon.pos - pygame.Vector2(diameter // 2, diameter // 2)
                 self.map.blit(circle_surface, blit_pos)
 
+def trilaterate(beacons: tuple[Beacon, Beacon, Beacon]) -> tuple[float, float]:
+    """
+    Find power center, used to get the initial guess for the optimizer
+    """
+    r1, r2, r3 = [i.get_distance() for i in beacons]
+    ((x1, y1), (x2, y2), (x3, y3)) = [[i.pos[0], i.pos[1]] for i in beacons]
+
+    a = (x2 - x1)
+    b = (y2 - y1)
+
+    r = (x3 - x1)
+    s = (y3 - y1)
+
+    # Check that the three beacons aren't colinear
+    if ( b*r - a*s ) == 0.0:
+        return (None, None)
+
+    c = (r1**2 - r2**2 + y2**2 + x2**2 - y1**2 - x1**2 ) / 2
+    t = (r1**2 - r3**2 + y3**2 + x3**2 - y1**2 - x1**2 ) / 2
+
+    # Use Cramer's rule
+    y = ( r*c - a*t ) / ( b*r - a*s )
+
+    if a == 0.0:
+        x = ( t - s*y ) / r
+    else:
+        x = ( c - b*y ) / a
+
+    return (x, y)
+
+def multilaterate(robot: Node, beacons: list[Beacon]) -> tuple[pygame.Vector2, float, float]:
+    estimates = [[],[]]
+    for i in itertools.combinations(beacons, 3):
+        est = trilaterate(i)
+
+        if est[0] == None:
+            print("There are colinear beacons")
+
+        else:
+            estimates[0].append(est[0])
+            estimates[1].append(est[1])
+    
+    mean_pos = np.average(estimates, axis=1)
+
+    estimated_pos = pygame.Vector2(float(mean_pos[0]), float(mean_pos[1]))
+    error = estimated_pos.distance_to(robot.pos)
+
+    # Calculate standar deviations to draw uncertainty ellipse
+
+    std_dev_x, std_dev_y = np.std(estimates, axis=1)
+
+    return (estimated_pos, float(std_dev_x), float(std_dev_y))
+
+
+def multilateration(map: pygame.Surface, robot: Node, beacons: list[Beacon], iterations: int, discard_threeshold: float) -> float:
+    est_x = []
+    est_y = []
+
+    for i in range(iterations):
+        estimate = multilaterate(robot, beacons)
+        if estimate[1] < discard_threeshold:
+            est_x.append(estimate[0][0])
+
+        if estimate[2] < discard_threeshold:
+            est_y.append(estimate[0][1])
+
+    if len(est_x) == 0.0 or len(est_y) == 0.0:
+        print("All on this lateration batch was discarted, measurements had too much noise")
+    else:
+        mean_x = float(np.average(est_x))
+        mean_y = float(np.average(est_y))
+
+        std_dev_x = float(np.std(est_x))
+        std_dev_y = float(np.std(est_y))
+
+        pygame.draw.ellipse(map, "green", pygame.Rect(mean_x - std_dev_x // 2, mean_y - std_dev_y // 2, std_dev_x, std_dev_y), 2)
+        pygame.draw.circle(map, "green", (mean_x, mean_y), 4)
+        pygame.draw.line(map, "green", (mean_x, mean_y), (robot.pos.x, robot.pos.y), 2)
+
+        return np.sqrt((mean_x - robot.pos.x)**2 + (mean_y - robot.pos.y)**2)
+
 robot = Node(pygame.Vector2(map.get_width() / 2, map.get_height() / 2), "red", 20, map)
 
 beacon1 = Beacon(pygame.Vector2(100, 300), "blue", 10, map, robot, add_noise=True, tx_power=-50, path_loss_exponent=2.0)
@@ -289,7 +394,7 @@ use_particle_filter = True
 show_particles = True
 show_distance_lines = True
 
-must_recalculate = True
+must_redraw = True
 
 nodes = [
     robot,
@@ -300,6 +405,10 @@ nodes = [
     beacon5
 ]
 
+recalculate_particles = True
+
+multilateration_error = 0.0
+
 while running:
     dt = clock.tick(60) / 1000
 
@@ -309,23 +418,34 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_c:
                 draw_circles = not draw_circles
-                must_recalculate = True
+                must_redraw = True
             if event.key == pygame.K_p:
                 use_particle_filter = not use_particle_filter
-                must_recalculate = True
+                must_redraw = True
+                if use_particle_filter:
+                    recalculate_particles = True
+                else:
+                    recalculate_particles = True
             if event.key == pygame.K_s:
                 show_particles = not show_particles
-                must_recalculate = False
+                recalculate_particles = False
+                must_redraw = True
             if event.key == pygame.K_l:
                 show_distance_lines = not show_distance_lines
-                must_recalculate = False
+                recalculate_particles = False
+                must_redraw = False
+            if event.key == pygame.K_t:
+                multilateration_error = multilateration(map, robot, beacons, 5, 100)
+                recalculate_particles = False
+                must_redraw = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 for i in nodes:
                     i.check_press()
                     if i.pressed:
-                        must_recalculate = True
+                        must_redraw = True
+                        recalculate_particles = False
                         break
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -333,13 +453,14 @@ while running:
                 for i in nodes:
                     if i.pressed:
                         i.drop()
-                        must_recalculate = True
+                        must_redraw = True
+                        recalculate_particles = True
                         break
 
         if event.type == pygame.QUIT:
             running = False
 
-    if must_recalculate:
+    if must_redraw:
         for i in nodes:
             i.drag()
 
@@ -358,24 +479,26 @@ while running:
             UI.draw_distance_lines(robot, beacons, "green")
     
         if use_particle_filter:
-            if must_recalculate:
+            if recalculate_particles:
                 estimated_pos = particle_filter.update()
                 error = estimated_pos.distance_to(robot.pos)
-
-            if show_particles:
-                particle_filter.draw(map)
             
-            # Draw estimated position
-            pygame.draw.circle(map, (255, 0, 255), (int(estimated_pos.x), int(estimated_pos.y)), 15, 2)
+            if show_particles:
+                particle_filter.draw_particles(map)
+            
+            particle_filter.draw_estimate(map)
+
             # Draw line from estimated to true position
-            pygame.draw.line(map, (255, 0, 0), robot.pos, estimated_pos, 2)
+            pygame.draw.line(map, "red", robot.pos, estimated_pos, 2)
 
             UI.draw_text(f"Estimation error: {error*SCALE:.1f} meters", pygame.Vector2(800, 60))
 
-        UI.draw_text("Press C to show/hide RSSI circles", pygame.Vector2(10, 15))
-        UI.draw_text("Press P to toggle particle filter", pygame.Vector2(410, 15))
-        UI.draw_text("Press S to show/hide particles", pygame.Vector2(10, 45))
-        UI.draw_text("Press L to show/hide distance lines", pygame.Vector2(410, 45))
+        UI.draw_text("Press C to show/hide RSSI confidence circles", pygame.Vector2(10, 15))
+        UI.draw_text("Press L to show/hide distance lines", pygame.Vector2(10, 45))
+        UI.draw_text("Press P to toggle particle filter", pygame.Vector2(450, 15))
+        UI.draw_text("Press S to show/hide particles", pygame.Vector2(450, 45))
+        UI.draw_text("Press T to multilaterate", pygame.Vector2(1670, 45))
+        UI.draw_text(f"Estimation error: {multilateration_error*SCALE:.1f}", pygame.Vector2(1670, 70))
 
     mouse_pos = pygame.mouse.get_pos()
 
@@ -392,6 +515,7 @@ while running:
     pygame.display.flip()
 
     if not pygame.mouse.get_pressed()[0]:
-        must_recalculate = False
+        must_redraw = False
+        recalculate_particles = False
 
 pygame.quit()
